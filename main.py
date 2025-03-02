cc6bd2fcb06e3b75b18c0daccddb2a3db0d420e6
hpooniah <harmanpreetp457@deltalearns.ca>
2025-03-01 16:02:26 -0800

    wokring on if they wnat to remove someone

--- main.py
+++ main.py
@@ -7,11 +7,7 @@ print(f"Let's get started {name}!")
 guest = []
 
 # Ask how many people are coming
-while true:
-    try:
-        number_invations = int(input("How many number of invations would you ike to make? "))
-    expect ValueError:
-        print("Only type a integer!")
+number_invations = int(input("How many number of invations would you ike to make? "))
 
 # A loop runs untill everyone has been added
 for num in range(number_invations):
@@ -21,15 +17,23 @@ for num in range(number_invations):
         print("The peroson has been added to the guest list")
 
 # Print a message to each person
-for num in range(num_people): 
+for num in range(number_invations): 
     print(f"Messaging {name_guest}")
     print(f"Hey {name_guest}! you have gotten a invatation to {name}'s dinner party")
 
 # Remove person from guest list
 while True:
     take_off = input("Is their anyone you would like to take off the guest list? [y] or [no]")
-    if take_off == "y"
-        eleminate = input("")
+    if take_off == "y":
+        eleminate = input("Please Enter the name of the person you want to take off from the guest list? ")
+        if eleminate in guest:
+            guest.remove(eleminate)
+            print(f"{eleminate} is no longer on the guest list")
+            print(guest)
+        else:
+             print(f"{eleminate} was already not on the guest list")
+    elif take_off == "n":
+         add = input(f"Is their anyone you would like to take {eleminate}'s spot? ")
 
 
 
