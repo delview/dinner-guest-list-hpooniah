3faec60047d1f1d01d4738763aa44ee6d169419e
hpooniah <harmanpreetp457@deltalearns.ca>
2025-03-01 16:40:32 -0800

    testing and fixing bugs

--- main.py
+++ main.py
@@ -12,29 +12,35 @@ number_invations = int(input("How many number of invations would you ike to make
 # A loop runs untill everyone has been added
 for num in range(number_invations):
     # Name of the guest that are coming
-        name_guest = input(" Name the guest that are invited: ")
-        guest.append(number_invations)
-        print("The peroson has been added to the guest list")
+    name_guest = input(f"Name the guest that are invited: #{num + 1}: ")
+    guest.append(name_guest)
+    print("The peroson has been added to the guest list")
 
 # Print a message to each person
-for num in range(number_invations): 
-    print(f"Messaging {name_guest}")
+print(f"Messaging {name_guest}")
+for guest_name in guest:
     print(f"Hey {name_guest}! you have gotten a invatation to {name}'s dinner party")
 
 # Remove person from guest list
 while True:
     take_off = input("Is their anyone you would like to take off the guest list? [y] or [no]")
     if take_off == "y":
-        eleminate = input("Please Enter the name of the person you want to take off from the guest list? ")
-        if eleminate in guest:
-            guest.remove(eleminate)
-            print(f"{eleminate} is no longer on the guest list")
+        eliminate = input("Please Enter the name of the person you want to take off from the guest list? ")
+        if eliminate in guest:
+            guest.remove(eliminate)
+            print(f"{eliminate} is no longer on the guest list")
             print(guest)
-        else:
-             print(f"{eleminate} was already not on the guest list")
-    elif take_off == "n":
-         add = input(f"Is their anyone you would like to take {eleminate}'s spot? ")
+            
+            # Ask if they want to replace the person
+            replace = input(f"Would you like to replace {eliminate} with someone else? [y] or [n]: ").lower()
+            if replace == "y":
+                join = input("Who would you like to invite instead? ").capitalize()
+                guest.append(join)
+                print(f"An invitation has been sent to {join}.")
+            elif replace == "n":
+                print("No replacement made.")
 
+         
 
 
 
